0e59c3b41e897e7f94705706050d9db37b3ec461
Michael Niedermaier <mn@cluetec.de>
2023-07-17 18:05:24 +0200

    working a bit

--- projects/ngx-tree/src/lib/ngcx-tree/ngcx-tree.component.ts
+++ projects/ngx-tree/src/lib/ngcx-tree/ngcx-tree.component.ts
@@ -1,7 +1,13 @@
 import { ArrayDataSource } from '@angular/cdk/collections';
-import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop';
+import {
+  CdkDrag,
+  CdkDragDrop,
+  CdkDropList,
+  DragDropModule,
+} from '@angular/cdk/drag-drop';
 import { CdkTreeModule, NestedTreeControl } from '@angular/cdk/tree';
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {
   faBook,
@@ -16,20 +22,31 @@ import { TREE_DATA } from '../util/mock-tree-nodes';
   templateUrl: 'ngcx-tree.component.html',
   styleUrls: ['ngcx-tree.component.scss'],
   standalone: true,
-  imports: [CdkTreeModule, DragDropModule, FontAwesomeModule],
+  imports: [CdkTreeModule, DragDropModule, FontAwesomeModule, CommonModule],
 })
-export class NgcxTreeComponent {
-  @Input() dataSource = new ArrayDataSource(TREE_DATA);
+export class NgcxTreeComponent implements OnChanges {
+  @Input() nodes = TREE_DATA;
 
+  dataSource = new ArrayDataSource(this.nodes);
   treeControl = new NestedTreeControl<TreeNode>((node) => node.children);
   dragging = false;
 
   faChevronRight = faChevronRight;
   faChevronDown = faChevronDown;
   faBook = faBook;
-
+  ngOnChanges(changes: SimpleChanges) {
+    this.dataSource = new ArrayDataSource(this.nodes);
+  }
   hasChild = (_: number, node: TreeNode) =>
     !!node.children && node.children.length > 0;
+
+  evenPredicate(parent?: TreeNode) {
+    return (drag: CdkDrag<TreeNode>, drop: CdkDropList<TreeNode>): boolean => {
+      console.log(parent?.title, drag.data.title, drop.data.title);
+      // console.log(treeControl.getLevel(node));
+      return !!parent;
+    };
+  }
 
   handleDrop(event: CdkDragDrop<string[]>) {
     console.log(event);
