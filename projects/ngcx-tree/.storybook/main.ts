83f2237a84c8a384104d13994aa3e1cc8b852f07
mn-42 <117656019+mn-42@users.noreply.github.com>
2024-12-11 15:01:30 +0100

    Angular 18 (#131)
    update angular to 18
    update storybook, eslint, prettier, peer deps
    use node v22

--- projects/ngcx-tree/.storybook/main.ts
+++ projects/ngcx-tree/.storybook/main.ts
@@ -1,13 +1,15 @@
 import type { StorybookConfig } from '@storybook/angular';
 const config: StorybookConfig = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook'
+  ],
   framework: {
     name: '@storybook/angular',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
+  docs: {},
 };
 export default config;
