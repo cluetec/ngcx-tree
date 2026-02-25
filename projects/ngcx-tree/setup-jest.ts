0ee28fa3a242db142a0eb0a6e1ee7089ccf0bfcc
Michael Niedermaier <mn@cluetec.de>
2026-02-25 12:00:46 +0100

    ng update @angular/core@20 @angular/cli@20

--- projects/ngcx-tree/setup-jest.ts
+++ projects/ngcx-tree/setup-jest.ts
@@ -1,10 +1,15 @@
+import { provideZoneChangeDetection, NgModule } from "@angular/core";
 import { TestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
 
+@NgModule({ providers: [ provideZoneChangeDetection() ] })
+export class ZoneChangeDetectionModule {}
+
+
 TestBed.initTestEnvironment(
-  BrowserDynamicTestingModule,
+  [ZoneChangeDetectionModule, BrowserDynamicTestingModule],
   platformBrowserDynamicTesting()
 );
