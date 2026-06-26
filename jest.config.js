28818af9210701916cc4d951433ed343269d25b5
Michael Niedermaier <mn@cluetec.de>
2026-06-26 09:30:30 +0200

    update versions

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setupJest.ts'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/projects/ngcx-tree/package.json'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
   transform: {
     '^.+\\.{ts}?$': [
